1607cfc6287ae859147ffde0bf18b1b76fb9895e
Jumail Mundekkat <mundekkat@hotmail.com>
2021-07-23 22:48:49 +1000

    Fix struct instantiation

--- framework/util/messenger_topic.go
+++ framework/util/messenger_topic.go
@@ -1,30 +1,37 @@
 package util
 
 // Callback function type for MessengerTopic
-type EventCallbackFunction func(data interface{}, topic *MessengerTopic)
+type EventCallbackFunction func(data interface{}, topic *messengerTopic)
 
 // Subscribable messaging bus
 // Allows data traffic between distinct parts of code
-type MessengerTopic struct {
+type messengerTopic struct {
 	// Subscribers, mapping names to callback functions
 	subscribers map[string]EventCallbackFunction
 	// Last data value passed to the topic
 	LastData interface{}
 }
 
+func New() messengerTopic {
+	topic := messengerTopic{
+		subscribers: make(map[string]EventCallbackFunction),
+	}
+	return topic
+}
+
 // Subscribe to topic with a given an identifier and a callback function
-func (topic *MessengerTopic) subscribe(subscriberId string, subscriber EventCallbackFunction) {
+func (topic *messengerTopic) subscribe(subscriberId string, subscriber EventCallbackFunction) {
 	topic.subscribers[subscriberId] = subscriber
 }
 
 // Unsubscribe a callback function from the topic, given a identifier
-func (topic *MessengerTopic) unsubscribe(subscriberId string) {
+func (topic *messengerTopic) unsubscribe(subscriberId string) {
 	delete(topic.subscribers, subscriberId)
 }
 
 // Notify all subscribed functions with given data
 // Subscribed functions are called concurrently in individual goroutines
-func (topic *MessengerTopic) notify(data interface{}) {
+func (topic *messengerTopic) notify(data interface{}) {
 	topic.LastData = data
 	for _, f := range topic.subscribers {
 		go f(data, topic)
