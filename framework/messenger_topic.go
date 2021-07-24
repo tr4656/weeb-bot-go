1e5968694b34c34d4929af21c92d14a2de749dbf
Jumail Mundekkat <mundekkat@hotmail.com>
2021-07-24 18:32:26 +1000

    Add Message generic interface; Change visibility of functions; Change package

--- framework/messenger_topic.go
+++ framework/messenger_topic.go
@@ -1,7 +1,7 @@
-package util
+package framework
 
 // Callback function type for MessengerTopic
-type EventCallbackFunction func(data interface{}, topic *messengerTopic)
+type EventCallbackFunction func(data *Message, topic *messengerTopic)
 
 // Subscribable messaging bus
 // Allows data traffic between distinct parts of code
@@ -12,6 +12,10 @@ type messengerTopic struct {
 	LastData interface{}
 }
 
+// Generic message data interface
+type Message interface{}
+
+// Create new instance of messengerTopic
 func New() messengerTopic {
 	topic := messengerTopic{
 		subscribers: make(map[string]EventCallbackFunction),
@@ -20,18 +24,18 @@ func New() messengerTopic {
 }
 
 // Subscribe to topic with a given an identifier and a callback function
-func (topic *messengerTopic) subscribe(subscriberId string, subscriber EventCallbackFunction) {
+func (topic *messengerTopic) Subscribe(subscriberId string, subscriber EventCallbackFunction) {
 	topic.subscribers[subscriberId] = subscriber
 }
 
 // Unsubscribe a callback function from the topic, given a identifier
-func (topic *messengerTopic) unsubscribe(subscriberId string) {
+func (topic *messengerTopic) Unsubscribe(subscriberId string) {
 	delete(topic.subscribers, subscriberId)
 }
 
 // Notify all subscribed functions with given data
 // Subscribed functions are called concurrently in individual goroutines
-func (topic *messengerTopic) notify(data interface{}) {
+func (topic *messengerTopic) Notify(data *Message) {
 	topic.LastData = data
 	for _, f := range topic.subscribers {
 		go f(data, topic)
