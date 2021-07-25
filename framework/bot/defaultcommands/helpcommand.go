025c6cf33a83c5e7e020614dafa9acab848b4fbe
Jumail Mundekkat <mundekkat@hotmail.com>
2021-07-25 15:22:09 +1000

    Move help message storage to HelpCommand

--- framework/bot/defaultcommands/helpcommand.go
+++ framework/bot/defaultcommands/helpcommand.go
@@ -6,17 +6,22 @@ import (
 	"github.com/tr4656/weeb-bot-go/framework/bot"
 )
 
-type HelpCommand struct{}
-
-func (h *HelpCommand) ProvideCommand() ([]string, bot.CommandFunction) {
-	return []string{"h", "help"}, handleHelp
+// Command to sent a help message
+type HelpCommand struct {
+	// Help message string to send on command
+	helpMessage string
 }
 
-func handleHelp(b *bot.Bot, cmd *bot.CommandArguments) {
+func (h *HelpCommand) ProvideCommand() ([]string, bot.CommandFunction) {
+	return []string{"h", "help"}, h.handleHelp
+}
+
+func (h *HelpCommand) handleHelp(b *bot.Bot, cmd *bot.CommandArguments) {
+	// Ensure the sole argument to the command is the bot name
 	if len(cmd.Arguments) == 0 ||
 		strings.EqualFold(cmd.Arguments[0], b.Name) {
 		return
 	}
 
-	b.Session.ChannelMessageSend(cmd.Message.ChannelID, *b.HelpMsg)
+	b.Session.ChannelMessageSend(cmd.Message.ChannelID, h.helpMessage)
 }
